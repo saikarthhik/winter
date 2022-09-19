d8238647780efe5663cba675aa5c21d45ddae97f
saikarthhik <71244295+saikarthhik@users.noreply.github.com>
2022-09-19 22:50:44 +0530

    Initial

---
+++ winter/Covid/assets/app.js
@@ -0,0 +1,45 @@
+url = "https://api.rootnet.in/covid19-in/stats/latest"
+
+bodyBlock = document.querySelector("#bodyBox")
+searchText = document.querySelector("#searchText")
+searchButton = document.querySelector("#searchButton")
+console.log(bodyBlock,searchButton);
+
+
+box = (x) => {
+    return `<div id = ${x.loc} class="flexClassForDivs">
+    <h3>${x.loc}<h3>
+    <h6 >Confirmed Cases : ${x.confirmedCasesIndian}</h6>
+    <h6>Deaths : ${x.deaths}</h6>
+    <h6>Discharged : ${x.discharged}</h6>
+</div>`
+}
+
+
+
+const filterElements = async() => {
+    const res = await fetch(url)
+    resData = await res.json()
+    searchData = searchText.value
+    console.log(resData.data.regional);
+    if(searchData in resData.data.regional){
+        alert("Found")
+    } 
+    else {
+        alert("Not Found")
+    }
+}
+
+
+const info = async() =>{
+    const res= await fetch(url)
+    data = await res.json()
+    for(i in data.data.regional){
+        // console.log(data.data.regional[i]);
+        x=box(data.data.regional[i])
+        bodyBlock.insertAdjacentHTML("beforeend", x)
+    }
+}
+
+searchButton.addEventListener('click', filterElements);
+info()
